82aad5d166ddf01b11de9700097295542ecdd388
ken-cherry <ken@cherrywebdev.com>
2024-01-24 22:32:22 -0600

    Edited: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,21 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Muscle Gains</a>
+      </div>
+      <div className="nav-center flex-1">
+        <div className="nav-links ">
+          {navigation.map((item) => {
+            const { id, to, text } = item;
+            return (
+              <NavLink to={to} className="nav-link" key={id}>
+                {text}
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex-none hidden">
         <button className="btn btn-square btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -22,26 +37,6 @@ const Navbar = () => {
           </svg>
         </button>
       </div>
-      <div className="flex-none">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      {/* <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
-      </div> */}
     </div>
   );
 };
