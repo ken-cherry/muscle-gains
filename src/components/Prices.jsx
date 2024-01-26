df6ed80c956692c113ea195f3a0ca34e2305f3f2
ken-cherry <ken@cherrywebdev.com>
2024-01-25 23:07:39 -0600

    Edited: data.js, Prices.jsx

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -3,17 +3,31 @@ import { CheckIcon } from "@heroicons/react/20/solid";
 
 import { prices } from "../data";
 
+const rates = prices[2];
+
+const {
+  id: id,
+  title: title,
+  text: text,
+  price: price,
+  includes: includes,
+} = rates;
+
+const validIncludes = Object.keys(rates)
+  .filter((key) => key.startsWith("includes") && rates[key] !== null)
+  .map((key) => rates[key]);
+
 const Prices = () => {
   return (
     <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div className="p-8 sm:p-10 lg:flex-auto">
-        <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-          Monthly - Basic
+        <h3
+          className="text-2xl font-bold tracking-tight text-gray-900"
+          key={id}
+        >
+          {title}
         </h3>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet
-          indis perferendis blanditiis repellendus etur quidem assumenda.
-        </p>
+        <p className="mt-6 text-base leading-7 text-gray-600">{text}</p>
         <div className="mt-10 flex items-center gap-x-4">
           <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
             What’s included
@@ -24,30 +38,28 @@ const Prices = () => {
           role="list"
           className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
         >
-          1hr workout 3 days a week.
-          {/* {prices.map((item) => {
-            const { id, includes } = item;
+          {/* 1hr workout 3 days a week. */}
+          {validIncludes.map((item, index) => {
             return (
-              <li key={id} className="flex gap-x-3">
+              <li key={item} className="flex gap-x-3">
                 <CheckIcon
                   className="h-6 w-5 flex-none text-indigo-600"
                   aria-hidden="true"
                 />
-                {includes}
+                {item}
+                {index < validIncludes.length - 1 ? "," : ""}
               </li>
             );
-          })} */}
+          })}
         </ul>
       </div>
       <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
         <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
           <div className="mx-auto max-w-xs px-8">
-            <p className="text-base font-semibold text-gray-600">
-              Pay once, own it forever
-            </p>
+            <p className="text-base font-semibold text-gray-600">{title}</p>
             <p className="mt-6 flex items-baseline justify-center gap-x-2">
               <span className="text-5xl font-bold tracking-tight text-gray-900">
-                $349
+                ${price}
               </span>
               <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
                 USD
