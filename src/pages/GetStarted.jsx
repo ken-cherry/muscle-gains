5331c83a872dfe78c1b47e615584e15e8c302e81
ken-cherry <ken@cherrywebdev.com>
2024-01-30 23:54:33 -0600

    Added: phone number to form input

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -6,7 +6,7 @@ const GetStarted = () => {
   return (
     <>
       <form>
-        <div className="space-y-12 px-2 bg-blue-400">
+        <div className="space-y-12 px-2 bg-blue-300">
           <div className="border-b border-gray-900/10 pb-12 py-2">
             <h2 className="text-base font-semibold leading-7 text-gray-900 text-center">
               Personal Information
@@ -52,12 +52,30 @@ const GetStarted = () => {
                 </div>
               </div>
 
-              <div className="sm:col-span-4">
+              <div className="sm:col-span-4 lg:col-span-3">
                 <label
                   htmlFor="email"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4 lg:col-span-3">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Phone Number
                 </label>
                 <div className="mt-2">
                   <input
@@ -180,7 +198,7 @@ const GetStarted = () => {
           </div>
         </div>
 
-        <div className=" flex justify-center pb-2 bg-blue-400">
+        <div className=" flex justify-center pb-2 bg-blue-300">
           <button
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
