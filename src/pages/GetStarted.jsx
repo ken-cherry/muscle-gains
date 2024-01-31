45dcaf13e0d042e1863361c828ed65fdbede6f92
ken-cherry <ken@cherrywebdev.com>
2024-01-30 21:58:25 -0600

    Styled: form getStarted

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -6,16 +6,16 @@ const GetStarted = () => {
   return (
     <>
       <form>
-        <div className="space-y-12">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">
+        <div className="space-y-12 ">
+          <div className="border-b border-gray-900/10 pb-12 py-2">
+            <h2 className="text-base font-semibold leading-7 text-gray-900 text-center">
               Personal Information
             </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
+            <p className="mt-1 text-sm leading-6 text-gray-600 text-center">
               Use a permanent address where you can receive mail.
             </p>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 ">
               <div className="sm:col-span-3">
                 <label
                   htmlFor="first-name"
@@ -145,18 +145,10 @@ const GetStarted = () => {
           </div>
 
           <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">
-              Notifications
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
-              We'll always let you know about important changes, but you pick
-              what else you want to hear about.
-            </p>
-
             <div className="mt-10 space-y-10">
               <fieldset>
                 <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  By Email
+                  Goals
                 </legend>
                 <div className="mt-6 space-y-6">
                   <div className="relative flex gap-x-3">
@@ -173,11 +165,8 @@ const GetStarted = () => {
                         htmlFor="comments"
                         className="font-medium text-gray-900"
                       >
-                        Comments
+                        Weight Loss
                       </label>
-                      <p className="text-gray-500">
-                        Get notified when someones posts a comment on a posting.
-                      </p>
                     </div>
                   </div>
                   <div className="relative flex gap-x-3">
@@ -194,11 +183,8 @@ const GetStarted = () => {
                         htmlFor="candidates"
                         className="font-medium text-gray-900"
                       >
-                        Candidates
+                        Lean Muscle
                       </label>
-                      <p className="text-gray-500">
-                        Get notified when a candidate applies for a job.
-                      </p>
                     </div>
                   </div>
                   <div className="relative flex gap-x-3">
@@ -215,65 +201,9 @@ const GetStarted = () => {
                         htmlFor="offers"
                         className="font-medium text-gray-900"
                       >
-                        Offers
+                        Bulk Up
                       </label>
-                      <p className="text-gray-500">
-                        Get notified when a candidate accepts or rejects an
-                        offer.
-                      </p>
                     </div>
-                  </div>
-                </div>
-              </fieldset>
-              <fieldset>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  Push Notifications
-                </legend>
-                <p className="mt-1 text-sm leading-6 text-gray-600">
-                  These are delivered via SMS to your mobile phone.
-                </p>
-                <div className="mt-6 space-y-6">
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="push-everything"
-                      name="push-notifications"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                    <label
-                      htmlFor="push-everything"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Everything
-                    </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="push-email"
-                      name="push-notifications"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                    <label
-                      htmlFor="push-email"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Same as email
-                    </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="push-nothing"
-                      name="push-notifications"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                    <label
-                      htmlFor="push-nothing"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      No push notifications
-                    </label>
                   </div>
                 </div>
               </fieldset>
@@ -281,18 +211,12 @@ const GetStarted = () => {
           </div>
         </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <button
-            type="button"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Cancel
-          </button>
+        <div className="mt-6 flex justify-center pb-2">
           <button
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Save
+            Submit
           </button>
         </div>
       </form>
